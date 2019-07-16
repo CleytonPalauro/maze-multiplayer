7c77b101d8bc1dc0e4ee437aba3c7f0b023734d6
Cleyton Palauro <cleytonresc@gmail.com>
2019-07-16 20:56:27 -0300

    Add files via upload

---
+++ Scripts/FileManager.cs
@@ -0,0 +1,213 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+/* "using System.IO"
+ * O namespace System.IO contém tipos que permitem ler e gravar 
+ * em arquivos e em fluxos de dados, além de tipos que
+ * dão suporte básico a diretórios e arquivos.
+ * https://docs.microsoft.com/pt-br/dotnet/api/system.io
+ */
+
+public class FileManager : MonoBehaviour
+{
+    [Header("File Section")]
+    public string fileName = "save.txt";
+
+    public enum fileExtension
+    {
+        txt, dat
+    }
+
+    public fileExtension myFile;
+
+    public string fileTitle = "Título do arquivo";
+
+    public string fileNote = "Observação";
+
+    [Header("TextAsset Section")]
+    public TextAsset myTextAsset;
+
+    [TextArea(4,20)]
+    public string myTextAssetString;
+    
+    void Start () { }
+
+    /// <summary>
+    /// Método para inserir a extensão do arquivo.
+    /// <para>Retorna uma string com o nome e extensão do arquivo de texto.</para>
+    /// </summary>
+    /// <param name="name">Recebe o nome do arquivo de texto.</param>
+    /// <returns>Retorna uma string com o nome e extensão do arquivo de texto.</returns>
+
+    public string FileExtension(string name)
+    {
+        string fullFileName;
+
+        switch (myFile)
+        {
+            case fileExtension.dat:
+                fullFileName = name + "dat";
+                break;
+            case fileExtension.txt:
+                fullFileName = name + "txt";
+                break;
+        }
+
+        return fullFileName;
+    }
+
+    /// <summary>
+    /// Método para Criar e gravar no arquivo.
+    /// </summary>
+    /// <param name="name">Recebe o nome do arquivo de texto.</param>
+    /// <param name="title">Recebe um titulo para ser inserido no cabeçalho.</param>
+    
+    public void CreateFile(string name, string title)
+    {
+        string fullName = FileExtension(name);
+
+        /* "Application.dataPath"
+         * Contém o caminho para a pasta de dados do jogo (somente leitura).
+         * O caminho depende de qual plataforma você está executando.
+         * Unity Editor: <caminho para a pasta do projeto>/Assets
+         * https://docs.unity3d.com/ScriptReference/Application-dataPath.html
+         */
+
+        string pathFile = Application.dataPath + "/" + fullName;
+
+        /* "File.Exists"
+         * Determina se o arquivo especificado existe.
+         * https://docs.microsoft.com/pt-br/dotnet/api/system.io.file.exists
+         */
+
+        if (!File.Exists(pathFile))
+        {
+            /* "File.WriteAllText"
+             * Cria um novo arquivo, grava a cadeia de caracteres
+             * especificada no arquivo e fecha o arquivo.
+             * Se o arquivo de destino já existir, ele será substituído.
+             * https://docs.microsoft.com/pt-br/dotnet/api/system.io.file.writealltext
+             */
+
+            File.WriteAllText(pathFile, title.ToUpper() + "\n");
+
+            Debug.Log("Arquivo " + fullName + " criado!");
+        }
+        else
+        {
+            Debug.Log("Arquivo " + fullName + " já existe!");
+        }
+    }
+
+    /// <summary>
+    /// Método para leitura do arquivo.
+    /// <para>Retorna uma string com o conteúdo do arquivo de texto.</para>
+    /// </summary>
+    /// <param name="name">Recebe o nome do arquivo de texto.</param>
+    /// <returns>Retorna uma string com o conteúdo do arquivo de texto.</returns>
+
+    public string ReadFile(string name)
+    {
+        string fullName = FileExtension(name);
+
+        string pathFile = Application.dataPath + "/" + fullName;
+
+        /* "File.ReadAllText"
+         * Abre um arquivo de texto, lê todo o texto do arquivo
+         * como uma cadeia de caracteres e o fecha.
+         * https://docs.microsoft.com/pt-br/dotnet/api/system.io.file.readalltext
+         */
+
+        string readfile = File.ReadAllText(pathFile);
+
+        return readfile;
+    }
+
+    /// <summary>
+    /// Método para atualizar o arquivo.
+    /// </summary>
+    /// <param name="name">Recebe o nome do arquivo de texto.</param>
+    /// <param name="note">Recebe uma nota para ser inserida.</param>
+    
+    public void UpdateFile(string name, string note)
+    {
+        string fullName = FileExtension(name);
+
+        string pathFile = Application.dataPath + "/" + fullName;
+
+        /* "System.DateTime.Now"
+         * Obtém um objeto DateTime definido como a data e hora atuais
+         * neste computador, expressas como a hora local.
+         * https://docs.microsoft.com/pt-br/dotnet/api/system.datetime.now
+         */
+
+        string content = "Login: " + System.DateTime.Now + " " + note + "\n";
+
+        /* "File.AppendAllText"
+         * Abre um arquivo, acrescenta a cadeia de caracteres especificada
+         * no arquivo e fecha o arquivo. Se o arquivo não existir,
+         * esse método criará um arquivo, gravará a cadeia de caracteres
+         * especificada no arquivo e fechará o arquivo.
+         * https://docs.microsoft.com/pt-br/dotnet/api/system.io.file.appendalltext
+         */
+
+        File.AppendAllText(path, content);
+
+        Debug.Log("Arquivo " + fullName + " atualizado!");
+    }
+
+    /// <summary>
+    /// Método para deletar o arquivo.
+    /// </summary>
+    /// <param name="name">Recebe o nome do arquivo de texto.</param>
+
+    public void DeleteFile(string name)
+    {
+        string fullName = FileExtension(name);
+
+        /* "Path.Combine"
+         * Este método concatena cadeias de caracteres individuais em uma 
+         * única cadeia de caracteres que representa um caminho de arquivo.
+         * https://docs.microsoft.com/pt-br/dotnet/api/system.io.path.combine
+         */
+
+        string path = Path.Combine(Application.dataPath, fullName);
+
+        if (File.Exists(path))
+        {
+            /* "File.Delete"
+             * Exclui o arquivo especificado no caminho "path".
+             * https://docs.microsoft.com/pt-br/dotnet/api/system.io.file.delete
+             */
+
+            File.Delete(path);
+
+            Debug.Log("Arquivo " + fullName + " deletado!");
+        }
+        else
+        {
+            Debug.Log("Arquivo " + fullName + " não encontrado!");
+        }
+    }
+
+    /// <summary>
+    /// Método para ler o conteúdo em texto do TextAsset "rawText".
+    /// <para>Retorna uma string com o conteúdo do TextAsset.</para>
+    /// </summary>
+    /// <param name="rawText">Recebe o TextAsset que será lido.</param>
+    /// <returns>Retorna uma string com o conteúdo do TextAsset.</returns> 
+
+    public string ReadTextAsset(TextAsset rawText)
+    {
+        /* "rawText.text"
+         * O conteúdo do texto do arquivo .txt como uma string. (Somente leitura)
+         * https://docs.unity3d.com/ScriptReference/TextAsset-text.html
+         */
+        
+        string myRawText = rawText.text;
+
+        return myRawText;
+    }
+}
---
+++ Scripts/MapGenerator.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/* "using System"
+ * O namespace System contém as classes fundamentais e
+ * as classes base que definem tipos de dados de referência e valor,
+ * eventos e manipuladores de eventos, interfaces,
+ * atributos e exceções de processamento comumente usados.
+ * https://docs.microsoft.com/pt-br/dotnet/api/system
+ */
+
+public class MapGenerator : MonoBehaviour
+{
+    [Header("Configurations")]
+    public string mapHolderName = "Generated Map";
+
+    [TextArea(4, 20)]
+    public string stringMap = "";
+
+    public Vector3 mapSize;
+
+    public Transform[] tilePrefabs;
+
+    [Header("Modifyers")]
+    public bool centralized = true;
+
+    [Range(-90, 90)]
+    public int rotateTo = 0;
+
+    [Range(1, 10)]
+    public int scaleTo = 1;
+
+    private int[] arrayList;
+
+    private int[,] matrizMap;
+
+	void Start ()
+    {
+        FillMap();
+
+        GenerateMap();
+    }
+	
+	void Update () { }
+
+    /// <summary>
+    /// Método para preencher a matriz virtual do mapa.
+    /// </summary>
+
+    public void FillMap()
+    {
+        /* "Array.ConvertAll"
+         * Converte uma matriz de um tipo para uma matriz de outro tipo.
+         * https://docs.microsoft.com/pt-br/dotnet/api/system.array.convertall
+         */
+
+        /* "int.Parse"
+         * Converte a representação de cadeia de caracteres de um número em
+         * um estilo e formato específicos da cultura especificados em
+         * seu equivalente de inteiro.
+         * https://docs.unity3d.com/ScriptReference/String.html
+         */
+
+        arrayList = Array.ConvertAll(stringMap.Split(','), int.Parse);
+
+        int width = (int)mapSize.x;
+
+        int height = (int)mapSize.z;
+
+        matrizMap = new int[width, height];
+
+        int count = 0;
+
+        for(int x = 0; x < width; x++)
+        {
+            for (int z = 0; z < height; z++)
+            {
+                if(count >= arrayList.Length)
+                {
+                    matrizMap[x, z] = 0;
+                }
+                else
+                {
+                    matrizMap[x, z] = arrayList[count];
+                }
+
+                count++;
+            }
+        }
+    }
+
+    /// <summary>
+    /// Método para construir o mapa na cena do jogo.
+    /// </summary>
+     
+    public void GenerateMap()
+    {
+        mapSize = new Vector3(mapSize.x, 0f, mapSize.z);
+
+        /* "transform.Find(name)"
+         * Encontra um filho pelo nome e retorna o "transform" do
+         * filho encontrado ou "null" se nenhum filho for encontrado.
+         * https://docs.unity3d.com/ScriptReference/Transform.Find.html
+         */
+
+        if (transform.Find(mapHolderName))
+        {
+            Destroy(transform.Find(mapHolderName).gameObject);
+            Debug.LogWarning("GameObject foi encontrado e destruido.");
+        }
+
+        /* "new GameObject(name)"
+         * Cria um novo GameObject com o nome com o qual o GameObject é criado.
+         * https://docs.unity3d.com/ScriptReference/GameObject-ctor.html
+         */
+
+        Transform mapHolder = new GameObject(mapHolderName).transform;
+
+        mapHolder.parent = transform;
+
+        // NORMALIZERS!
+        int myCenter = 0;
+        float myOffset = 0;
+
+        if (centralized)
+        {
+            myCenter = 2;
+            myOffset = 0.5f;
+        }
+        else
+        {
+            myCenter = 1;
+            myOffset = 1f;
+        }
+
+        for(int x = 0; x < mapSize.x; x++)
+        {
+            for(int z = 0; z < mapSize.z; z++)
+            {
+                Vector3 tilePosition =
+                    new Vector3(
+                        -mapSize.x / myCenter + myOffset + x,
+                        0f,
+                        -mapSize.z / myCenter + myOffset + z);
+
+                int localIndex = 0;
+
+                if (matrizMap[x,z] < tilePrefabs.Length)
+                {
+                    localIndex = matrizMap[x, z];
+                }
+                else
+                {
+                    localIndex = 0;
+                }
+
+                Transform newTile =
+                    Instantiate(
+                        tilePrefabs[localIndex],
+                        tilePosition,
+                        Quaternion.Euler(Vector3.up * rotateTo)
+                    );
+
+                newTile.localScale = Vector3.one;
+
+                newTile.parent = mapHolder;
+            }
+        }
+
+        mapHolder.localScale = Vector3.one * scaleTo;
+    }
+}
